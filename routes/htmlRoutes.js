de93486425d153d94b33d079a52a818f11e09997
NaomiMae <44372104+NaomiMae@users.noreply.github.com>
2019-03-05 20:39:18 -0500

    added login (#4)

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,5 +1,5 @@
 var db = require("../models");
-
+var path = require("path");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
@@ -10,7 +10,14 @@ module.exports = function(app) {
       });
     });
   });
-
+  app.get("/signup", function(req, res) {
+    // If the user already has an account send them to the members page
+   console.log ("hello");
+   if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
@@ -19,7 +26,20 @@ module.exports = function(app) {
       });
     });
   });
-
+  app.get("/signup", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
+  app.get("/login", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
