bfb3a3ab2cc1fb8d90276a0f19e0a800aa4c6ad6
Spatula Pete <petewx26@gmail.com>
2019-03-05 21:19:32 -0500

    add validation to input, modify api-routes to match db name

--- models/recipe.js
+++ models/recipe.js
@@ -1,6 +1,12 @@
 module.exports = function(sequelize, DataTypes) {
   var Recipe = sequelize.define("Recipe", {
-    recipeName: DataTypes.STRING,
+    recipeName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 255]
+      }
+    },
     ingredient_one: DataTypes.STRING,
     ingredient_two: DataTypes.STRING,
     ingredient_three: DataTypes.STRING,
@@ -11,7 +17,13 @@ module.exports = function(sequelize, DataTypes) {
     ingredient_eight: DataTypes.STRING,
     ingredient_nine: DataTypes.STRING,
     ingredient_ten: DataTypes.STRING,
-    description: DataTypes.TEXT,
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1, 2000]
+      }
+    },
     needToBuy: DataTypes.BOOLEAN
   });
   return Recipe;
